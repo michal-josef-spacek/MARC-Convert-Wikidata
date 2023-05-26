a807b04a3d324f92c471acac9f4ebe63fe3fd2a4
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-05-26 14:11:21 +0200

    First version of doc

--- Wikidata.pm
+++ Wikidata.pm
@@ -152,3 +152,193 @@ sub _leader {
 1;
 
 __END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+MARC::Convert::Wikidata - Conversion class between MARC file to Wikibase::Datatype item.
+
+=head1 SYNOPSIS
+
+ use MARC::Convert::Wikidata;
+
+ my $obj = MARC::Convert::Wikidata->new(%params);
+ my $object = $obj->object;
+ my $type = $obj->type;
+ my $wikidata = $obj->wikidata;
+
+=head1 DESCRIPTION
+
+Original intent of this class was conversion from MARC records in National Library of the
+Czech Republic to Wikidata. The conversion is not simple, this mean that many
+things are concrete for this concrete national library.
+
+=head1 METHODS
+
+=head2 C<new>
+
+ my $obj = MARC::Convert::Wikidata->new(%params);
+
+Constructor.
+
+=over 8
+
+=item * C<callback_cover>
+
+Cover callback
+
+Default value is undef.
+
+=item * C<callback_lang>
+
+Language callback.
+
+Default value is undef.
+
+=item * C<callback_people>
+
+People callback.
+
+Default value is undef.
+
+=item * C<callback_publisher_place>
+
+Place of publication Wikidata lookup callback.
+
+Default value is undef.
+
+=item * C<callback_publisher_name>
+
+Publisher Wikidata lookup callback.
+
+Default value is undef.
+
+=item * C<callback_series>
+
+Book series Wikidata lookup callback.
+
+Default value is undef.
+
+=item * C<date_retrieved>
+
+Retrieved date.
+
+Default value is undef.
+
+=item * C<marc_record>
+
+MARC::Record object.
+
+It's required.
+
+=back
+
+Returns instance of object.
+
+=head2 C<object>
+
+ my $object = $obj->object;
+
+Get data object created from MARC record.
+
+Returns MARC::Convert::Wikidata::Object instance.
+
+=head2 C<type>
+
+ my $type = $obj->type;
+
+Process MARC record and detect which record type is.
+Supported values are: monograph, audiobook and periodical.
+
+Returns string.
+
+=head2 C<wikidata>
+
+ my $wikidata = $obj->wikidata;
+
+Process conversion from MARC record to Wikibase::Datatype::Item which is
+possible to load to Wikidata.
+
+Returns Wikibase::Datatype instance.
+
+=head1 ERRORS
+
+ new():
+         From Class::Utils::set_params():
+                 Unknown parameter '%s'.
+         Parameter 'marc_record' is required.
+         Parameter 'marc_record' must be a MARC::Record object.
+
+ type():
+         Unsupported item with leader '%s'.
+
+ wikidata():
+         Item '%s' doesn't supported.
+         Unsupported item with leader '%s'.
+
+=head1 EXAMPLE
+
+=for comment filename=get_random_day.pl
+
+ use strict;
+ use warnings;
+
+ use File::Temp;
+ use MARC::Convert::Wikidata;
+
+ # Object.
+ my $obj = MARC::Convert::Wikidata->new;
+
+ # TODO
+
+ # Output like:
+ # TODO
+
+=head1 DEPENDENCIES
+
+L<Class::Utils>,
+L<Error::Pure>,
+L<MARC::Convert::Wikidata::Item::AudioBook>,
+L<MARC::Convert::Wikidata::Item::BookEdition>,
+L<MARC::Convert::Wikidata::Item::Periodical>,
+L<MARC::Convert::Wikidata::Transform>,
+L<Scalar::Util>.
+
+=head1 SEE ALSO
+
+=over
+
+=item L<MARC::Record>
+
+Perl extension for handling MARC records
+
+=item L<Wikibase::Datatype::Item>
+
+Wikibase item datatype.
+
+=back
+
+=head1 REPOSITORY
+
+L<https://github.com/michal-josef-spacek/MARC-Convert-Wikidata>
+
+=head1 AUTHOR
+
+Michal Josef Špaček L<mailto:skim@cpan.org>
+
+L<http://skim.cz>
+
+=head1 LICENSE AND COPYRIGHT
+
+© 2021-2023 Michal Josef Špaček
+
+BSD 2-Clause License
+
+=head1 VERSION
+
+0.01
+
+=cut
