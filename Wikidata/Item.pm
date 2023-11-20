2bc390df83d8364f05bff0c3fd3e3a5cc5d10347
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-11-20 14:17:10 +0100

    Prepare for 0.07 version

--- Wikidata/Item.pm
+++ Wikidata/Item.pm
@@ -18,7 +18,7 @@ use Wikibase::Datatype::Value::Quantity;
 use Wikibase::Datatype::Value::String;
 use Wikibase::Datatype::Value::Time;
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Constructor.
 sub new {
