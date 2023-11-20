be170705471f82ae65f802143eb54b7bae42dffb
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2023-11-20 14:33:03 +0100

    Add example for clean_number_of_pages() to doc

--- Wikidata/Utils.pm
+++ Wikidata/Utils.pm
@@ -700,6 +700,26 @@ Returns string or undef.
  # Edition number: Druhé vydání
  # Cleaned edition number: 2
 
+=head1 EXAMPLE4
+
+=for comment filename=clean_number_of_pages.pl
+
+ use strict;
+ use warnings;
+
+ use MARC::Convert::Wikidata::Utils qw(clean_number_of_pages);
+
+ my $number_of_pages = '575 s. ;';
+ my $cleaned_number_of_pages = clean_number_of_pages($number_of_pages);
+
+ # Print out.
+ print "Number of pages: $number_of_pages\n";
+ print "Cleaned number of pages: $cleaned_number_of_pages\n";
+
+ # Output:
+ # Number of pages: 575 s. ;
+ # Cleaned number of pages: 575
+
 =head1 DEPENDENCIES
 
 L<Exporter>,
