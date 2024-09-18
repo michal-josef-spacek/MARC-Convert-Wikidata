ce82e79a75c5b5fdafb8d18182f54e542f0b7f25
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-09-18 21:40:57 +0200

    Prepare for 0.16 version

--- Wikidata/Item.pm
+++ Wikidata/Item.pm
@@ -26,7 +26,7 @@ Readonly::Hash our %EXT_ID_MAPPING => (
 	'lccn' => 'P243',
 );
 
-our $VERSION = 0.15;
+our $VERSION = 0.16;
 
 # Constructor.
 sub new {
--- Wikidata/Transform.pm
+++ Wikidata/Transform.pm
@@ -37,7 +37,7 @@ Readonly::Hash our %PEOPLE_TYPE => {
 	'trl' => 'translators',
 };
 
-our $VERSION = 0.15;
+our $VERSION = 0.16;
 
 # Constructor.
 sub new {
