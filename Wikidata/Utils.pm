4e78cc77aa78556a8842d6dad72ada48273dbe0c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-03-19 22:38:41 +0100

    Prepare for 0.26 version

--- Wikidata/Utils.pm
+++ Wikidata/Utils.pm
@@ -15,7 +15,7 @@ Readonly::Array our @EXPORT_OK => qw(clean_cover clean_date clean_issn clean_edi
 	clean_title look_for_external_id);
 Readonly::Array our @COVERS => qw(hardback paperback);
 
-our $VERSION = 0.25;
+our $VERSION = 0.26;
 our $DEBUG = 0;
 
 sub clean_cover {
@@ -872,6 +872,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.25
+0.26
 
 =cut
